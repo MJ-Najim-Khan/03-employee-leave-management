4fad78576895a2b609fb63eab5d2f1e62f1c3def
MJ-Najim-Khan <mj.mdnajimkhan@gmail.com>
2026-09-25 16:25:20 +0530

    chore: configure production CORS and HTTPS for Route53

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,8 +27,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", 
-                   "http://127.0.0.1:5173"],
+    allow_origins=[
+        "https:d2hzg5hj2i2lr0.cloudfront.net"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
